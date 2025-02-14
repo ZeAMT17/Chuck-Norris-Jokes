4f6a656cd9896f50578d3a01354e88b01efbc4d2
Jose Melendez <joseantoniomelendezteran@gmail.com>
2025-02-13 21:21:33 -0400

    changes made to the button text and h1 background

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ export function Card({ frase, error, refetch }) {
     <div className="card" style={{ width: "18rem" }}>
       <h1>{frase}</h1>
       {error && <p>{error}</p>}
-      <button onClick={refetch}>Nuevo chiste</button>
+      <button onClick={refetch}>New Joke</button>
     </div>
   );
 }
